867d864f1da04542efcdc5b20c3827d5cf2ff7aa
robsouthgate4 <robsouthgate4@gmail.com>
2018-03-25 19:36:27 +0100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+var GitRevisionPlugin = require('git-revision-webpack-plugin')
+
+module.exports = env => {
+    let output;
+    if (!env) {
+        output = {
+            filename: '[name].bundle.js',
+            path: path.resolve(__dirname, 'dist'),
+            publicPath: ''
+        }
+    } else {
+        output = {
+            filename: `[name].bundle.js`,
+            path: path.resolve(__dirname, `demos/demo_[git-revision-branch]`),
+            publicPath: ''
+        }
+    }
+
+    return {
+        context: __dirname,
+        node: {
+            __filename: true
+        },
+        entry: './src/app/index.js',
+        devtool: 'inline-source-map',
+        devServer: {
+            contentBase: './src/public',
+            port: 9000
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: __dirname + "/src/public/index.html",
+                inject: 'body'
+            }),
+            new GitRevisionPlugin({
+                branch: true
+            })
+        ],
+        output,
+        module: {
+            rules: [
+                {
+                    test: /\.scss$/,
+                    use: [{
+                        loader: "style-loader" // creates style nodes from JS strings
+                    }, {
+                        loader: "css-loader" // translates CSS into CommonJS
+                    }, {
+                        loader: "sass-loader" // compiles Sass to CSS
+                    }]
+                },
+                {
+                    test: /\.(ogg|mp3|jpe?g|png|gif|obj|svg|wav|mpe?g)$/i,
+                    loader: 'file-loader'
+                },
+                {
+                    test: /\.js$/,
+                    use: 'babel-loader',
+                    exclude: [
+                        /node_modules/
+                    ]
+                },
+                {
+                    test: /node_modules/,
+                    loader: 'ify-loader'
+                },
+                {
+                    enforce: 'post',
+                    test: /\.js$/,
+                    loader: 'ify-loader'
+                }
+            ]
+        }
+    }
+}
+
